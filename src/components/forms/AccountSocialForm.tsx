eee9b06b3780ed95797a0cf4e17427a83fcba12b
Victor Fazekas <victorfazekas3@gmail.com>
2024-10-10 19:12:55 -0300

    changed the svgs

--- src/components/forms/AccountSocialForm.tsx
+++ src/components/forms/AccountSocialForm.tsx
@@ -1,32 +1,87 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import {
-  DiscordLogoIcon,
-  EnvelopeOpenIcon,
-  GitHubLogoIcon,
-  InstagramLogoIcon,
-} from "@radix-ui/react-icons";
 import SectionBox from "../SectionBox";
 import { Button } from "../ui/button";
-import { Facebook, Instagram, Plus, Smartphone, Youtube } from "lucide-react";
+import { Plus } from "lucide-react";
+import Image from "next/image";
 
 export default function AccountSocialForm() {
   const buttons = [
-    { key: "email", label: "E-mail", icons: <EnvelopeOpenIcon /> },
-    { key: "mobile", label: "Mobile", icons: <Smartphone /> },
-    { key: "instagram", label: "Instagram", icons: <InstagramLogoIcon /> },
-    // { key: "facebook", label: "Facebook", icons: <Facebook /> },
-    { key: "discord", label: "Discord", icons: <DiscordLogoIcon /> },
-    { key: "tiktok", label: "Tiktok", icons: <EnvelopeOpenIcon /> },
-    // { key: "youtube", label: "Youtube", icons: <Youtube /> },
-    { key: "whatsapp", label: "Whatsapp", icons: <EnvelopeOpenIcon /> },
-    { key: "github", label: "Github", icons: <GitHubLogoIcon /> },
+    {
+      key: "email",
+      label: "E-mail",
+      icons: (
+        <Image src="/icons/email.svg" width={24} height={24} alt="email" />
+      ),
+    },
+    {
+      key: "instagram",
+      label: "Instagram",
+      icons: (
+        <Image
+          src="/icons/instagram.svg"
+          width={24}
+          height={24}
+          alt="instagram"
+        />
+      ),
+    },
+    {
+      key: "facebook",
+      label: "Facebook",
+      icons: (
+        <Image
+          src="/icons/facebook.svg"
+          width={24}
+          height={24}
+          alt="Facebook"
+        />
+      ),
+    },
+    {
+      key: "discord",
+      label: "Discord",
+      icons: (
+        <Image
+          src="/icons/discord.svg"
+          width={24}
+          height={24}
+          alt="discord"
+        />
+      ),
+    },
+    {
+      key: "tiktok",
+      label: "Tiktok",
+      icons: (
+        <Image src="/icons/tiktok.svg" width={24} height={24} alt="tiktok" />
+      ),
+    },
+    {
+      key: "whatsapp",
+      label: "Whatsapp",
+      icons: (
+        <Image
+          src="/icons/whatsApp.svg"
+          width={24}
+          height={24}
+          alt="whatsapp"
+        />
+      ),
+    },
+    {
+      key: "github",
+      label: "Github",
+      icons: (
+        <Image src="/icons/github.svg" width={24} height={24} alt="github" />
+      ),
+    },
   ];
+
   return (
     <SectionBox>
-      <div className="flex flex-col max-w-[8em]">
+      <div className="flex flex-col max-w-[10em] gap-4">
         {buttons.map((b) => (
           <Button key={b.key} className="gap-2">
-            <div className="flex w-4 h-4">{b.icons}</div>
+            <div>{b.icons}</div>
             <span>{b.label}</span>
             <Plus size={12} />
           </Button>
